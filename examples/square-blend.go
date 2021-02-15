ca0dcc8322f73b518d8e39d1bdd60e79fce910a6
Ken  Ledford <kenledford@Kens-MBP.attlocal.net>
2021-02-15 11:55:15 -0800

    initial commit

---
+++ examples/square-blend.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"math/rand"
+	"time"
+
+	gen "github.com/guitar-slug/gen-art/pkg"
+)
+
+func main() {
+
+	width := 700
+	height := 1000
+
+	//seed
+	rand.Seed(time.Now().UnixNano())
+
+	img := image.NewRGBA(image.Rectangle{
+		image.Point{0, 0},
+		image.Point{width, height},
+	})
+
+	gen.SetBackgroundColor(img, color.RGBA{100, 250, 250, 250})
+
+	for i := 0; i < 50; i++ {
+
+		locX := gen.RandInt(0, width)
+		locY := gen.RandInt(0, height)
+		siz := gen.RandInt(1, 200)
+
+		col := color.RGBA{
+			gen.RandU8(0),
+			gen.RandU8(0),
+			gen.RandU8(0),
+			255,
+		}
+
+		gen.SquareBlend(img, image.Point{locX, locY}, siz, col)
+	}
+
+	gen.WritePng("../gallery/square-blend.png", img)
+}
